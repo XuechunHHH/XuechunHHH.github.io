e4f96b87eb80002870543e937cd9529d6e8a5da5
XuechunHHH <108777908+xuechunhhhh@users.noreply.github.com>
2023-03-03 21:44:06 -0800

    hw8 backend

--- hw8/backend/index.js
+++ hw8/backend/index.js
@@ -5,6 +5,7 @@ const ngeohash=require('ngeohash');
 
 // [START gae_node_request_example]
 const express = require('express');
+const SpotifyWebApi = require("spotify-web-api-node");
 
 const app = express();
 const token_ticket = 'hkd6lIBkV2PJxAAC4ElXY8XvTAHT2wmL';
@@ -22,7 +23,7 @@ var dic={
 app.use(cors()).use(cookie());
 
 app.get('/', (req, res) => {
-    res.send("jzlsb")
+    res.send("jzlsb");
 });
 
 app.get('/suggest', (req, res) => {
@@ -133,7 +134,297 @@ app.get('/search', (req, res) => {
 });
 
 app.get('/event', (req, res) => {
-    var lat = req.query.lat;
+    var id = req.query.id;
+    var url = 'https://app.ticketmaster.com/discovery/v2/events/'+id;
+    axios.get(url, {
+        params: {
+            apikey: token_ticket
+        }
+    })
+        .then(function (response) {
+            var data = response.data;
+            try {
+                var name = data['name'];
+            }catch (e){
+                var name = null;
+            }
+            try {
+                var url = data["url"];
+            }catch (e){
+                var url = null;
+            }
+            try {
+                var localDate = data["dates"]["start"]["localDate"];
+            }catch (e){
+                var localDate = null;
+            }
+            try {
+                var attractions = data["_embedded"]["attractions"];
+            }catch (e){
+                var attractions = null;
+            }
+            var artist = [];
+            for (var i=0;i<attractions.length;++i){
+                try {
+                    artist.push(attractions[i]["name"]);
+                }catch (e){
+                    artist.push(null);
+                }
+            }
+            try {
+                var venue = data["_embedded"]["venues"][0]["name"];
+            }catch (e){
+                var venue = null;
+            }
+            var genre = [];
+            try {
+                genre.push(data["classifications"][0]["segment"]["name"]);
+            }catch (e){
+                genre.push(null);
+            }
+            try {
+                if (!genre.includes(data["classifications"][0]["genre"]["name"]) && data["classifications"][0]["genre"]["name"] !== 'Undefined'){
+                    genre.push(data["classifications"][0]["genre"]["name"]);
+                }
+            }catch (e){
+                genre.push(null);
+            }
+            try {
+                if (!genre.includes(data["classifications"][0]["subGenre"]["name"]) && data["classifications"][0]["subGenre"]["name"] !== 'Undefined'){
+                    genre.push(data["classifications"][0]["subGenre"]["name"]);
+                }
+            }catch (e){
+                genre.push(null);
+            }
+            try {
+                if (!genre.includes(data["classifications"][0]["type"]["name"]) && data["classifications"][0]["type"]["name"] !== 'Undefined'){
+                    genre.push(data["classifications"][0]["type"]["name"]);
+                }
+            }catch (e){
+                genre.push(null);
+            }
+            try {
+                if (!genre.includes(data["classifications"][0]["subType"]["name"]) && data["classifications"][0]["subType"]["name"] !== 'Undefined'){
+                    genre.push(data["classifications"][0]["subType"]["name"]);
+                }
+            }catch (e){
+                genre.push(null);
+            }
+            try {
+                var pricerange = data["priceRanges"][0];
+            }catch (e){
+                var pricerange = null;
+            }
+            try {
+                var status = data["dates"]["status"]["code"];
+            }catch (e){
+                var status = null;
+            }
+            try {
+                var seatmap = data["seatmap"]["staticUrl"];
+            }catch (e){
+                var seatmap = null;
+            }
+            var dic = {
+                name: name,
+                localDate: localDate,
+                artist: artist,
+                venue: venue,
+                genre: genre,
+                pricerange: pricerange,
+                status: status,
+                url: url,
+                seatmap: seatmap
+            };
+            res.status(200).send(dic);
+        })
+        .catch(function (error) {
+            res.status(201).send(error.message);
+        })
+});
+
+app.get('/artist', (req, res) => {
+    var name = req.query.name;
+
+    var SpotifyWebApi = require('spotify-web-api-node');
+// credentials are optional
+    var spotifyApi = new SpotifyWebApi({
+        clientId: 'c4bb7efcffd34c10a562caa8f9a53bd7',
+        clientSecret: '30988567a57542298ca240ee2d77971a',
+        redirectUri: 'http://localhost:8080/callback'
+    });
+
+    spotifyApi.clientCredentialsGrant().then(
+        function(data) {
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.searchArtists(name)
+                .then(function(data) {
+                    try {
+                        var returnNames = data.body["artists"]["items"];
+                        // console.log(returnNames);
+                    }catch (e){
+                        var returnNames = {};
+                    }
+                    var cnt = 0;
+
+                    for (var i=0;i<returnNames.length;++i){
+                        cnt ++;
+                        var artist = {};
+                        if (returnNames[i]["name"].toUpperCase() === name.toUpperCase()){
+                            artist["name"] = returnNames[i]["name"];
+                            try {
+                                artist["followers"] = returnNames[i]["followers"]["total"];
+                            }catch (e){
+                                artist["followers"] = null;
+                            }
+                            try {
+                                artist["popularity"] = returnNames[i]["popularity"];
+                            }catch (e){
+                                artist["popularity"] = null;
+                            }
+                            try {
+                                artist["url"] = returnNames[i]["external_urls"]["spotify"];
+                            }catch (e){
+                                artist["url"] = null;
+                            }
+                            try {
+                                artist["image"] = returnNames[i]["images"][0]["url"];
+                            }catch (e){
+                                artist["image"] = null;
+                            }
+                            try {
+                                artist["id"] = returnNames[i]["id"];
+                            }catch (e){
+                                artist["id"] = null;
+                            }
+                            res.status(200).send(artist);
+                            break;
+                        }
+                    }
+                    if (cnt === returnNames.length){
+                        res.status(200).send({});
+                    }
+
+                    }, function(err) {
+                    console.error(err);
+                });
+        },
+        function(err) {
+            console.log(
+                'Something went wrong when retrieving an access token',
+                err.message
+            );
+        });
+
+});
+
+app.get('/album', (req, res) => {
+    var id = req.query.id;
+
+    var SpotifyWebApi = require('spotify-web-api-node');
+// credentials are optional
+    var spotifyApi = new SpotifyWebApi({
+        clientId: 'c4bb7efcffd34c10a562caa8f9a53bd7',
+        clientSecret: '30988567a57542298ca240ee2d77971a',
+        redirectUri: 'http://localhost:8080/callback'
+    });
+
+    spotifyApi.clientCredentialsGrant().then(
+        function(data) {
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+            spotifyApi.getArtistAlbums(id)
+                .then(function(data) {
+                    try {
+                        var imagesRe = data.body["items"];
+                    }catch (e){
+                        var imagesRe = {};
+                    }
+                    var album = [];
+                    var cnt = imagesRe.length;
+                    if (cnt>3){
+                        cnt = 3;
+                    }
+                    for (var i=0;i<cnt;++i){
+                        album.push(imagesRe[i]["images"][0]["url"]);
+                    }
+                    res.status(200).send(album);
+                }, function(err) {
+                    res.send([]);
+                });
+        },
+        function(err) {
+            console.log(
+                'Something went wrong when retrieving an access token',
+                err.message
+            );
+        });
+
+});
+
+app.get('/venue', (req, res) => {
+    var str = req.query.str;
+    axios.get('https://app.ticketmaster.com/discovery/v2/venues', {
+        params: {
+            apikey: token_ticket,
+            keyword: str
+        }
+    })
+        .then(function (response) {
+            var data = response.data;
+            var venue = {};
+            try {
+                data = data["_embedded"]["venues"][0];
+            }catch (e){
+                res.status(200).send({});
+            }
+            try {
+                venue["address"] = data["address"]["line1"];
+            }catch (e){
+                venue["address"] = null;
+            }
+            try {
+                venue["city"] = data["city"]["name"];
+            }catch (e){
+                venue["city"] = null;
+            }
+            try {
+                venue["state"] = data["state"]["name"];
+            }catch (e){
+                venue["state"] = null;
+            }
+            try {
+                venue["name"] = data["name"];
+            }catch (e){
+                venue["name"] = null;
+            }
+            try {
+                venue["tel"] = data["boxOfficeInfo"]["phoneNumberDetail"];
+            }catch (e){
+                venue["tel"] = null;
+            }
+            try {
+                venue["openHour"] = data["boxOfficeInfo"]["openHoursDetail"];
+            }catch (e){
+                venue["openHour"] = null;
+            }
+            try {
+                venue["generalRule"] = data["generalInfo"]["generalRule"];
+            }catch (e){
+                venue["generalRule"] = null;
+            }
+            try {
+                venue["childRule"] = data["generalInfo"]["childRule"];
+            }catch (e){
+                venue["childRule"] = null;
+            }
+
+            res.status(200).send(venue);
+        })
+        .catch(function (error) {
+            res.status(201).send(error.message);
+        })
 });
 
 // Start the server
